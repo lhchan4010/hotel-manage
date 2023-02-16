411f39cc6e1d1825a1f7aee83481f8daf3be3063
lhchan4010 <lhchan4010@gmail.com>
2023-02-16 22:39:36 +0900

    server setup

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log("hello");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// The GraphQL schema
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+    Query: {
+        hello: () => "world",
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+console.log(`🚀 Server ready at ${url}`);
